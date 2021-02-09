1ef9aab733af79b887d79162cb19e52e87db1cb0
Socio777 <crispinjar@gmail.com>
2021-02-09 23:57:53 +0100

    Números pares de 0 a 100 con while y con for

--- src/paquetePruebaGit/EjemploGit.java
+++ src/paquetePruebaGit/EjemploGit.java
@@ -8,11 +8,18 @@ public class EjemploGit {
 		System.out.println("Hola amigos de Git");
 		
 		int x=0;
+		int y=0;
 		
 		while (x<100) {
 			System.out.println(x);
 			x+=2;
 		}
+		
+		for(int i=0; i<100;i++) {
+			System.out.println(i);
+			i++;;
+		}
+		
 
 	}
 
